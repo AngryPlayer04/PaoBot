922240b9952e192dc133b4f789028fce3421a3c1
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-06-29 18:42:00 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from itertools import cycle
 
 # Get configuration.json
 with open("configuration.json", "r") as config: 
-	data = json.load(config)
-	token = data["token"]
-	prefix = data["prefix"]
-	
+  data = json.load(config)
+  token = data["token"]
+  prefix = data["prefix"]
+  
 
 # Intents
 intents = discord.Intents.default()
@@ -19,42 +19,39 @@ status = cycle(['Prefix: p.', 'Pão', 'Bread'])
 
 @bot.event
 async def on_ready():
-	print("Pão com manteiga")
-	change_status.start()
-<<<<<<< Updated upstream
-=======
-	print('Tá on!!')
->>>>>>> Stashed changes
+  print("Pão com manteiga")
+  change_status.start()
+
 
 @tasks.loop(seconds = 20)
 async def change_status():
-	await bot.change_presence(activity=discord.Game(next(status)))
-			
+  await bot.change_presence(activity=discord.Game(next(status)))
+      
 
 @bot.command(name='clear', help=': this command will clear msgs')
 async def clear(ctx, amount = 5):
-	await ctx.channel.purge(limit=amount)
+  await ctx.channel.purge(limit=amount)
 
 #Ping
 @bot.command()
 async def ping(ctx):
-		msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
-		await ctx.send(msg)
-	
+    msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
+    await ctx.send(msg)
+  
 
 @bot.event
 async def on_message(message):
-	if message.content == "Pão":
-		await message.channel.send(':bread:')
-	if message.content == "pão":
-		await message.channel.send(':bread:')
-	if message.content == "bread":
-		await message.channel.send(':bread:')
-	if message.content == "Bread":
-		await message.channel.send(':bread:')
-	if message.content == "Oãp":
-		await message.channel.send(':bread:')
-	await bot.process_commands(message)
+  if message.content == "Pão":
+    await message.channel.send(':bread:')
+  if message.content == "pão":
+    await message.channel.send(':bread:')
+  if message.content == "bread":
+    await message.channel.send(':bread:')
+  if message.content == "Bread":
+    await message.channel.send(':bread:')
+  if message.content == "Oãp":
+    await message.channel.send(':bread:')
+  await bot.process_commands(message)
     
 
 bot.run(token)
