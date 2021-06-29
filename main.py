9fb8ec77582635b2d608c338106a6a300ac2549d
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-06-29 13:26:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ status = cycle(['Prefix: p.', 'Pão', 'Bread'])
 @bot.event
 async def on_ready():
 	change_status.start()
-	print('O {bot.user} está on!!')
+	print('Tá on!!')
 
 @tasks.loop(seconds = 20)
 async def change_status():
@@ -31,6 +31,13 @@ async def change_status():
 async def clear(ctx, amount = 5):
 	await ctx.channel.purge(limit=amount)
 
+#Ping
+@bot.command()
+async def ping(ctx):
+		msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
+		await ctx.send(msg)
+	
+
 @bot.event
 async def on_message(message):
 	if message.content == "Pão":
@@ -38,6 +45,10 @@ async def on_message(message):
 	if message.content == "pão":
 		await message.channel.send(':bread:')
 	if message.content == "bread":
+		await message.channel.send(':bread:')
+	if message.content == "Bread":
+		await message.channel.send(':bread:')
+	if message.content == "Oãp":
 		await message.channel.send(':bread:')
 	await bot.process_commands(message)
     
