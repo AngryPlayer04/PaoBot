d97bad197213b7733d014b4f7c3ab8d008b919a8
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-04 12:21:05 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import platform
 from datetime import datetime 
 import pytz
+import random
 from itertools import cycle
 
 # Get configuration.json
@@ -47,6 +48,13 @@ async def ping(ctx):
     msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
     await ctx.send(msg)
   
+@bot.comman(name= 'receita', help='this command send a random recipe')
+async def receita(ctx):
+  lin = "https://www.tudogostoso.com.br/receita/72313-pao-caseiro-facil.html",\
+ "https://www.tudogostoso.com.br/receita/79996-pao-de-queijo-3-ingredientes.html", \
+ "https://www.tudogostoso.com.br/receita/83-pao-de-batata.html", \
+ "https://www.tudogostoso.com.br/receita/105067-pao-recheado.html"
+  await ctx.reply(random.choice(lin))
 
 @bot.event
 async def on_message(message):
