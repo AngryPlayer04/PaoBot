1e476949e481878250c25c8cee6f30f0de46abfa
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-04 12:24:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def ping(ctx):
     msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
     await ctx.send(msg)
   
-@bot.comman(name= 'receita', help='this command send a random recipe')
+@bot.command(name= 'receita', help='this command send a random recipe')
 async def receita(ctx):
   lin = "https://www.tudogostoso.com.br/receita/72313-pao-caseiro-facil.html",\
  "https://www.tudogostoso.com.br/receita/79996-pao-de-queijo-3-ingredientes.html", \
