19245de9fdcd48424c80ffacb26c2805cb68dfb0
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-12 11:16:50 -0300

    Add files via upload

--- Cogs/print.py
+++ Cogs/print.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 class Print(commands.Cog, name='Print'):
     """Send a screenshot of the replyed message"""
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot):
         self.bot= bot
 
-@commands.command()
-async def print(self, ctx):
-    await ctx.reply('Testando')
+    @commands.command()
+    async def print(self, ctx):
+        await ctx.reply('Testando')
 
 
-def setup(bot: commands.Bot):
+def setup(bot):
     bot.add_cog(Print(bot))
