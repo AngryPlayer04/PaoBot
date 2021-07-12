5de40270af71ad0c38510d7bfda00f3106ff02ed
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-12 12:58:53 -0300

    Add files via upload

--- Cogs/print.py
+++ Cogs/print.py
@@ -1,12 +1,19 @@
 import discord
 from discord.ext import commands
+from PIL import Image, ImageFont, ImageDraw
 
 class Print(commands.Cog):
     def __init__(self, bot):
         self.bot= bot
     @commands.command()
     async def print(self, ctx):
-        await ctx.reply('Testando')
+        imagem = Image.new('RGBA', (700, 200), 'grey')
+        fonte = ImageFont.truetype('unisans.ttf', 24)
+        quote = get_quote()
+        w, h = fonte.getsize(quote)
+        draw = ImageDraw.Draw(imagem)
+        draw.text(((500-w)/2, (500-h)/2), quote, font=fonte, fill='white')
+        await ctx.reply(Image.show)
 
 
 def setup(bot):
