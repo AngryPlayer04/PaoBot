edba00580992b7758accb7b2ecba55bc72399308
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-12 21:22:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_ready():
 @bot.event 
 async def on_ready():
   await bot.wait_until_ready()
-  user = (319963626108878848)
+  user = 319963626108878848
   await user.send('Ready!',tts=True)
   
 
