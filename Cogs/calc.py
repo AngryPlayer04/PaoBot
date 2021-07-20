198a7aa5c3b992c83e3ee1c8f84f6a4593c0801e
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-20 13:50:25 -0300

    calc

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -1,14 +1,15 @@
-import discord
-from discord.ext import commands
+import discord 
+import os 
+import math 
+from discord.ext import commands 
 
 class Calc(commands.Cog):
     def __init__(self, bot):
-        self.bot= bot
+        self.bot = bot 
     @commands.command()
-    async def print(self, ctx, *, expressao):
-        cal = eval(expressao)
-        await ctx.reply(cal)
-
+    async def calc(ctx, *, expression):
+        c = eval(expression)
+        await ctx.reply(c)
 
 def setup(bot):
     bot.add_cog(Calc(bot))
