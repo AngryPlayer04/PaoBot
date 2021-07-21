f51c1b12285d1dbafd63a359181fd1700d7c5865
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-21 15:44:47 -0300

    dadadadadadadadad

--- Cogs/wiki.py
+++ Cogs/wiki.py
@@ -15,21 +15,22 @@ class Wiki(commands.Cog):
         request = msg[2:]
         request = "".join(request)
         error = None
+        
+        wikicontent = wikipedia.search(request, results=20, suggestion = False )
+
         if 'wikipedia' in query:
             speak("Searching")
             query = ("wikipedia","")
             result = wikipedia.summary(f'{query}', sentences=2)
             print(result)
-        wikicontent = wikipedia.search(request, results=20, suggestion = False )
 
-        
         if not wikicontent:
             wikicontent = "Desculpe, não há resultados para `{}`.".format(request)
             embed = discord.Embed(title = "Resultados da pesquisa no Wikipedia:", color = "#e8cda2", descriptionn = wikicontent)
             embed.set_thumbnail(url = "https://www.wikipedia.org/static/images/project-logos/{}wiki.png".format(current_language))
             await ctx.reply(embed = embed)
 
-        if wikicontent:
+        elif wikicontent:
             embed = discord.Embed(title = "Resultado da pesquisa no Wikipedia", color = 0, description = "\n".join(wikicontent))
             embed.set_thumbnail(url = "https://www.wikipedia.org/static/images/project-logos/{}wiki.png".format(current_language))
             await ctx.reply(embed=embed)
