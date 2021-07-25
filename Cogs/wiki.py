191325c57045f2b039ec5dfbeffd9f7045f592ab
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-25 13:21:55 -0300

    wiki

---
+++ Cogs/wiki.py
@@ -0,0 +1,22 @@
+import discord 
+from discord.ext import commands
+import wikipedia
+
+
+class Wiki(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+    @bot.command()
+    async def wiki(ctx, userInput):
+        try: 
+            await ctx.reply(format(wikipedia.summary(userInput, sentences = 5)))
+        except wikipedia.exceptions.DisambiguationError as e:
+            await ctx.reply(format("Erro: {0}".format(e)))
+            await ctx.send ("Erro: muitos resultados, tente novamente com mais detalhes.")
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Wiki(bot))
