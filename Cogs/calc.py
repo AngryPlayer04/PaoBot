86e33c9eec221d68160dcd49ddb9b69b10f10ce4
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-07-26 20:17:37 -0300

    .

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -3,14 +3,14 @@ import os
 import math 
 from discord.ext import commands 
 
-class Cal(commands.Cog):
+class Calc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
-    @commands.command(name = "Cal", help = "Calculadora")
-    async def cal(self, ctx, *, expression):
+    @commands.command(name = "Calc", help = "Calculadora")
+    async def calc(self, ctx, *, expression):
         c = float(eval(expression))
         await ctx.reply(":abacus:  **|**  {} é igual a: `{:.3}`".format(expression, c))
 
 
 def setup(bot):
-    bot.add_cog(Cal(bot))
+    bot.add_cog(Calc(bot))
