3d43f8b04d2e97aafebd5690e7e6fcd3609817bb
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-01 02:35:40 -0300

    update calc.py

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -1,15 +1,15 @@
 
-import discord
-from discord.ext import commands
-import math
+#import discord
+#from discord.ext import commands
+#import math
 
-class Calculators(commands.Cog, name="calculator"):
-	def __init__(self, bot:commands.Bot):
-		self.bot = bot
-	@commands.command(ctx, *, expression):
-	  soma = eval(expression)
-	  await ctx.reply (soma)
+#class Calculators(commands.Cog, name="calculator"):
+#	def __init__(self, bot:commands.Bot):
+#		self.bot = bot
+#	@commands.command(ctx, *, expression):
+#	  soma = eval(expression)
+#	  await ctx.reply (soma)
 	  
 	  
-def setup(bot):
-  bot.add_cog(Calculators)
+#def setup(bot):
+#  bot.add_cog(Calculators)
