a8da610040a1583aae565831fe427a75d0cb6131
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-01 23:44:42 -0300

    calc added again

---
+++ Cogs/calc.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import math
+
+class Calculators(commands.Cog, name = "calculator"):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+    @commands.command(aliases = "calcu"): 
+    async def calc(ctx, *, express):
+        soma = eval(express)
+        await ctx.reply(soma)
+
+def setup(bot):
+    bot.add_cog(Calculators(bot))
