d25506c81bad6387defaa6e46c019721e2097ddc
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-02 15:20:36 -0300

    ifn ot

--- Cogs/ram.py
+++ Cogs/ram.py
@@ -25,7 +25,7 @@ class Owneronly(commands.Cog, name = "owneronly"):
             tr = discloud.total_ram()
             await ctx.reply("Usando {} de ram".format(r)) # 1GB
 
-        else:
+        if not:
             await ctx.reply("Desculpe, mas você não é o meu dono.")
 
 def setup(bot):
