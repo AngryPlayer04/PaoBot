6ef19a362124022f82ecbd18db5ba5777286a25e
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-02 14:45:36 -0300

    te

--- Cogs/ram.py
+++ Cogs/ram.py
@@ -15,14 +15,22 @@ class Owneronly(commands.Cog, name = "owner only commands"):
     @commands.command()
     @commands.is_owner()
     async def ram(self, ctx):
-        r = discloud.ram()
-         # 100/1024MB
-        # dados do uso de RAM
-        ur = discloud.using_ram()
-         # 100MB
-        # dados do total de RAM disponÃ­vel
-        tr = discloud.total_ram()
-        await ctx.reply("Usando {} de ram".format(r)) # 1GB
+
+        if commands.is_owner:
+            async def ram(self, ctx):
+                r = discloud.ram()
+            # 100/1024MB
+            # dados do uso de RAM
+                ur = discloud.using_ram()
+            # 100MB
+            # dados do total de RAM disponÃ­vel
+                tr = discloud.total_ram()
+                await ctx.reply("Usando {} de ram".format(r)) # 1GB
+
+        else:
+            await ctx.reply("Desculpe, mas você não é o meu dono.")
+
+    
 
 def setup(bot):
     bot.add_cog(Owneronly(bot))
