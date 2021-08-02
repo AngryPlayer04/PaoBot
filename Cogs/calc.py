1e207956d0b9ad17dc2430b51030dd2f9ec76b57
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-01 23:51:53 -0300

    Add files via upload

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -5,7 +5,7 @@ import math
 class Calculators(commands.Cog, name = "calculator"):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
-    @commands.command(aliases = "calcu"): 
+    @commands.command(aliases = "calcu")
     async def calc(ctx, *, express):
         soma = eval(express)
         await ctx.reply(soma)
