482fba601a066e1e813906e13c7d2da3ab929d6d
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-02 14:59:06 -0300

    owner

--- Cogs/ram.py
+++ Cogs/ram.py
@@ -6,31 +6,32 @@ import json
 with open("configuration.json", "r") as config: 
 	data = json.load(config)
 	prefix = data["prefix"]
+owner_id = 319963626108878848
+bot = commands.Bot(prefix, owner_id)
 
-bot = commands.Bot(prefix, owner_id = 319963626108878848)
-
-class Owneronly(commands.Cog, name = "owner only commands"):
+class Owneronly(commands.Cog, name = "owneronly"):
     def __init__(self, ctx,):
         self.bot = bot 
     @commands.command()
     @commands.is_owner()
     async def ram(self, ctx):
-
-        if commands.is_owner:
-            async def ram(self, ctx):
-                r = discloud.ram()
+        if ctx.message.author.id == owner_id:
+            r = discloud.ram()
             # 100/1024MB
             # dados do uso de RAM
-                ur = discloud.using_ram()
+            ur = discloud.using_ram()
             # 100MB
             # dados do total de RAM disponÃ­vel
-                tr = discloud.total_ram()
-                await ctx.reply("Usando {} de ram".format(r)) # 1GB
+            tr = discloud.total_ram()
+            await ctx.reply("Usando {} de ram".format(r)) # 1GB
 
         else:
             await ctx.reply("Desculpe, mas você não é o meu dono.")
 
-    
+    #@owneronly.error
+    #async def owneronly_error(error, ctx):
+        i#f isinstance(error, isnot)
+        
 
 def setup(bot):
     bot.add_cog(Owneronly(bot))
