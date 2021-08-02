3994ff9166b6c6b9dff7197fd8abd0a03f228a8d
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-02 14:26:58 -0300

    ram

---
+++ Cogs/ram.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import discloud
+
+bot = commands.Bot(owner_id = 319963626108878848)
+
+class Owneronly(commands.Cog, name = "owner only commands"):
+    def __init__(self, ctx,):
+        self.bot = bot 
+    @commands.command()
+    @commands.is_owner()
+    async def ram(self, ctx):
+        r = discloud.ram()
+         # 100/1024MB
+        # dados do uso de RAM
+        ur = discloud.using_ram()
+         # 100MB
+        # dados do total de RAM disponÃ­vel
+        tr = discloud.total_ram()
+        await ctx.reply("Usando {} de ram".format(r)) # 1GB
+
+def setup(bot):
+    bot.add_cog(Owneronly(bot))
