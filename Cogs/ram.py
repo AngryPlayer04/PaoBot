1de9a700aee4da698c48d1d79fc0c78a4208227d
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-02 21:08:41 -0300

    nnnj

--- Cogs/ram.py
+++ Cogs/ram.py
@@ -29,7 +29,7 @@ class Owneronly(commands.Cog, name = "owneronly"):
             # 1GB
             await ctx.reply("Usando {} de ram".format(r)) 
 
-        except(error, CheckFailure):
+        except:
             await ctx.reply("Desculpe, mas você não é o meu dono.")
 
 def setup(bot):
