9703e498b2d7067baab31c504ffde800d50bc5d7
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-14 19:44:43 -0300

    B

--- Cogs/onCommandError.py
+++ Cogs/onCommandError.py
@@ -27,10 +27,7 @@ class OnCommandErrorCog(commands.Cog, name="on command error"):
 		elif isinstance(error, MissingPermissions):
 			await ctx.send(error.text)
 			print(error)
-		elif isinstance(error, CheckFailure):
-			await ctx.send(error)
-			print(error)
-		elif isinstance(NotOwner, CheckFailure,error ):
+		elif isinstance(NotOwner, CheckFailure, error):
 			await ctx.reply("Desculpe, mas você não tem as permissões necessárias para usar esse comando.")
 			print(error)
 		else:
