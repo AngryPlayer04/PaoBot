3c4ca5bea6ae7aaac9ff20502572e8e9e2592d67
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-15 12:27:50 -0300

    dadadadaddaa

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import numexpr as ne
+from currency_converter import CurrencyConverter as c
 
 class Calculators(commands.Cog, name = "calculator"):
     def __init__(self, bot:commands.Bot):
@@ -13,6 +14,13 @@ class Calculators(commands.Cog, name = "calculator"):
 
         except (RuntimeError, OverflowError, ValueError, SyntaxError, NameError, TypeError, ZeroDivisionError):
             await ctx.reply("Desculpe, eu não posso calcular `{}` ou ocorreu um erro desconhecido.".format(express))
+    
+    @commands.command()
+    async def dolar(ctx):
+        cambio = c.convert(1, 'USD', 'BRL')
+        await ctx.reply ("O dólar está {} do Real.".format(cambio))
+
+
 
 
 def setup(bot):
