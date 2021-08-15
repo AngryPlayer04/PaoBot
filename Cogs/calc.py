e14bc18018e000af8fe74c9a2cbe51a8c2e99564
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-15 12:36:23 -0300

    adaawaaawasdasda

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -16,7 +16,7 @@ class Calculators(commands.Cog, name = "calculator"):
             await ctx.reply("Desculpe, eu não posso calcular `{}` ou ocorreu um erro desconhecido.".format(express))
     
     @commands.command()
-    async def dolar(ctx):
+    async def dolar(self, ctx):
         cambio = c.convert(1, 'USD', 'BRL')
         await ctx.reply ("O dólar está {} do Real.".format(cambio))
 
