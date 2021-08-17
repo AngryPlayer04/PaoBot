f01e0c2639482f89d6b61aafe727ad9fb16e5cc5
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-17 20:03:57 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands 
 import random 
 
-bot = commands.Bot()
+
 
 class Funny(commands.Cog, name = "Funny Commands"):
     def __init__(self, bot:commands.Bot):
