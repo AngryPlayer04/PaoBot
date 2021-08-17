28c06810572cd62834bdd22fd3cac3bcba310724
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-17 11:24:49 -0300

    aaaaaaaa

--- Cogs/ram.py
+++ Cogs/ram.py
@@ -16,18 +16,18 @@ class AdminOnly(commands.Cog, name = "adminonly"):
         self.bot = bot 
 
     @commands.command()
-    async def clear(ctx, amount = 5):
-        await ctx.purge(limit=amount)
+    async def clear(self, ctx, amount = 5):
+        await ctx.channel.purge(limit=amount)
         
     @commands.Cog.listener()
-    async def on_message(message):
+    async def on_message(self, message):
         if message.content in ["Pão", "pão", "bread", "Bread", "Oãp"]:
             bread = 'https://tenor.com/view/falling-bread-bread-gif-19081960'
             await message.reply(bread)
             await bot.process_commands(message)
 
     @commands.command()
-    async def ping(ctx):
+    async def ping(self, ctx):
         msg = "Pong <a:paopula:858815343072903178> `{0} ms`!".format(int(bot.latency * 1000))
         await ctx.reply(msg)
 
