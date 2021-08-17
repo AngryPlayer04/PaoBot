071728b0c1b116977204b25efb7fa215d9eb76ce
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-17 17:51:52 -0300

    cu

--- Cogs/onCommandError.py
+++ Cogs/onCommandError.py
@@ -10,29 +10,12 @@ class OnCommandErrorCog(commands.Cog, name="on command error"):
         
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx:commands.Context, error:commands.CommandError):
-		if isinstance(error, commands.CommandOnCooldown):
-			day = round(error.retry_after/86400)
-			hour = round(error.retry_after/3600)
-			minute = round(error.retry_after/60)
-			if day > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+str(day)+ "day(s)")
-			elif hour > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+str(hour)+ " hour(s)")
-			elif minute > 0:
-				await ctx.send('This command has a cooldown, be sure to wait for '+ str(minute)+" minute(s)")
-			else:
-				await ctx.send(f'This command has a cooldown, be sure to wait for {error.retry_after:.2f} second(s)')
-		elif isinstance(error, CommandNotFound):
+		if isinstance(error, CommandNotFound):
 			return
-		elif isinstance(error, MissingPermissions):
-			await ctx.send(error.text)
-			print(error)
-		elif isinstance(error, CheckFailure):
+
+		if isinstance(error, CheckFailure, MissingPermissions):
 			await ctx.reply("Desculpe, mas você não tem as permissões necessárias para usar esse comando.")
-			print(error)
-		else:
-			print(error) 
-			
+			print (error)
 		raise error
 
 def setup(bot):
