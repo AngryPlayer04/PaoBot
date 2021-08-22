01081def9b558b955f0988f8cad28158d2471b22
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-22 14:03:17 -0300

    hmmm

--- Cogs/util.py
+++ Cogs/util.py
@@ -10,7 +10,7 @@ class Utiliies(commands.Cog, name = "Utilities"):
     async def lyrics(self, ctx, *, name, artist):
         TOKEN = 'e-jVTE4L7DSjxhi2r8K4hbEw88c0m_BOBLp-6kMozrfWyVyuxdp4mBbbUUzu24l9'
         genius = Genius(TOKEN)
-        song = genius.search_song(name, artist)
+        song = genius.search_song('name''artist')
         embed=discord.Embed(title=name, description=song, color=0x0067c2)
         embed.set_author(name=artist)
         embed.set_thumbnail(url='https://camo.githubusercontent.com/2f1afecef9033d21cc42b61763e9de2f28dcebfde75576d317c9dce72eb5c592/68747470733a2f2f696d616765732e67656e6975732e636f6d2f38656436363963616464393536343433653239633730333631656334663337322e31303030783130303078312e706e67')
