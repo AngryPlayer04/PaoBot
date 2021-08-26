ab99d0bf0852ca840fe1eafcc469f8197fe57138
AngryPlayer04 <60411891+AngryPlayer04@users.noreply.github.com>
2021-08-26 20:58:30 -0300

    aaaaaaaaa

--- Cogs/util.py
+++ Cogs/util.py
@@ -6,12 +6,6 @@ class Utiliies(commands.Cog, name = "Utilities"):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
     
-    @commands.command()
-    async def lyrics(self, ctx, title, artist):
-        TOKEN = 'e-jVTE4L7DSjxhi2r8K4hbEw88c0m_BOBLp-6kMozrfWyVyuxdp4mBbbUUzu24l9'
-        genius = Genius(TOKEN)
-        song = genius.search_song(title, artist)
-        await ctx.reply(song.lyrics['lyrics'])
 
 
 
