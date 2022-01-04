882916969c0826fe246ddebe13ec3b7858797884
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-01-03 21:42:09 -0300

    i think it ill work

--- main.py
+++ main.py
@@ -32,12 +32,18 @@ for filename in os.listdir('Cogs'):
 
 @bot.event
 async def on_ready():
+  status= cycle(['p.help', 'Pão', 'Bread', f'Estou em {len(bot.guilds)} servidores'])
+
   tz_BR = pytz.timezone('America/Sao_Paulo') 
   datetime_BR = datetime.now(tz_BR)
   print ('============================================')
   print (f'Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
   #print (len(bot.guilds))
   print ('===============================')
+
+  async def change_status():
+    await bot.change_presence(activity=discord.Game(next(status)))
+
   change_status.start() 
 
 @tasks.loop(seconds=15)
