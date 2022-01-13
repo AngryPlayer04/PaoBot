a238e254d413ccc17089a0e4c0bb43bc53eae6f1
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-01-13 02:02:33 -0300

    update admin.py

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -29,7 +29,7 @@ class AdminOnly(commands.Cog, name = "adminonly"):
     @commands.command()
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
-        await ctx.send(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
+        await ctx.reply(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
 
 def setup(bot):
     bot.add_cog(AdminOnly(bot))
