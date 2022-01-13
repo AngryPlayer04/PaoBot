7a85efcef448930b6c7757e2934613f0e2ec7730
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-01-13 15:40:56 -0300

    restart command

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -29,11 +29,23 @@ class PermOnly(commands.Cog, name = "Permonly"):
         tr = discloud.total_ram()
         ur = discloud.using_ram()
         await ctx.reply(f'Usando {ur}/{tr} de ram')
+    
+    @commands.command()
+    @commands.is_owner()
+    async def restart(self,ctx):
+        result = requests.post("https://discloud.app/status/bot/850123093077917716/restart", headers={"api-token": "disctoken"}).json()
+
+    async def get_data():
+        async with aiohttp.ClientSession() as ses:
+            async with ses.post("https://discloud.app/status/bot/850123093077917716/restart", headers={"api-token": "disctoken"}) as res:
+                return await res.json()
 
     @commands.command()
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
         await ctx.reply(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
+
+
 
     
 
