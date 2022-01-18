ac18da48ddeeeae46b57cdfb37455f8f7f837c75
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-01-18 19:02:54 -0300

    lyrics

--- Cogs/util.py
+++ Cogs/util.py
@@ -1,10 +1,28 @@
 import discord
 from discord.ext import commands
+import lyricsgenius as genius
+
+api = genius.Genius("Us6gcFcURe9Y85HL6hPY7TRJWQBdVq8WXYiGnJEKokQFobG0v5bzO8MM2Kjy04xU")
 
 
 class Utiliies(commands.Cog, name = "Utilities"):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
+
+    @commands.command()
+    async def lyrics(self, name="Lyrics", alias="letras"):
+        try:
+            a = input(self.bot.reply('Diga o nome do artista'))
+            a
+            s = input(self.bot.send('Qual o nome da música?'))
+            s 
+            lyrics = api.search_song(s, a).lyrics
+            await self.bot.send(lyrics)
+
+        except:
+            await self.bot.send('Não encontrei a música solicitada, verifque se digitou corretamente.')
+
+
     
 
 
