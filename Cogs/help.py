695b9518d3cb8b7eba5c1a4a2c506a818dc41259
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-02-02 13:33:38 -0300

    dadadad

--- Cogs/help.py
+++ Cogs/help.py
@@ -1,13 +1,12 @@
-from threading import Thread
 import disnake
 from disnake.ext import commands
 
 class HelpCommand(commands.MinimalHelpCommand):
     async def send_pages(self):
-        destination = self.get_destination(disnake.TextChannel, disnake.DMChannel, Thread)
+        destination = self.get_destination()
         for page in self.paginator.pages:
             emby = disnake.Embed(description=page)
-            await destination.reply (embed=emby)
+            await destination.send(embed=emby)
 
 
     
