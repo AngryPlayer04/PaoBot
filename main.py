20a9bae3f170bdae6c75dc0e5f12c4f4971843bf
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-02-02 14:07:19 -0300

    cu

--- main.py
+++ main.py
@@ -27,22 +27,8 @@ current_time = datetime.now()
 tz_BR = pytz.timezone('America/Sao_Paulo') 
 datetime_BR = datetime.now(tz_BR)
 
-@tasks.loop(seconds=11)
-async def ligado():
-  user = [319963626108878848]
-  for id in user:
-    member = await bot.fetch_user(id)
-    try:
-      await member.send(f'{member.mention} Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
-    except:
-      pass
+@bot.event
 
-
-async def status_task():
-    while True:
-        await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching,
-                name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.online)
-        await asyncio.sleep(1800)
 
 @bot.event
 async def on_ready():
@@ -61,6 +47,23 @@ for filename in os.listdir('Cogs'):
   
   bot.loop.create_task(status_task())
   ligado.stop()
+
+@tasks.loop(seconds=11)
+async def ligado():
+  user = [319963626108878848]
+  for id in user:
+    member = await bot.fetch_user(id)
+    try:
+      await member.send(f'{member.mention} Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
+    except:
+      pass
+
+
+async def status_task():
+    while True:
+        await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching,
+                name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.online)
+        await asyncio.sleep(1800)
 
 
 @tasks.loop(hours = 8)
