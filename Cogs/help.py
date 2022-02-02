d38e38fcb245371dc19b0ec9d6c4720dbdd218bf
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-02-02 12:55:54 -0300

    hm?

--- Cogs/help.py
+++ Cogs/help.py
@@ -1,8 +1,12 @@
+import disnake
 from disnake.ext import commands
 
 class HelpCommand(commands.MinimalHelpCommand):
     def get_command_signature(self, command):
-        return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
+        return '{1.qualified_name} {1.signature}'.format(self, command)
+
+
+    
 
 class HelpCog(commands.Cog, name = 'Help'):
     '''Mostra os comandos e suas funções'''
