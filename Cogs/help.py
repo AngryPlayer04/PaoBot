2517147d9f91f851a4c293327bdb2ac9dde9945c
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-02-02 12:36:43 -0300

    help handler

---
+++ Cogs/help.py
@@ -0,0 +1,21 @@
+from disnake import commands
+
+class HelpCommand(commands.MinimalHelpCommand):
+    def get_command_signature(self, command):
+        return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
+
+class HelpCog(commands.Cog, name = 'Help'):
+    '''Mostra os comandos e suas funções'''
+
+    def __init__(self, bot):
+        self._original_help_command = bot.help_command
+        bot.help_command = HelpCommand()
+        bot.help_command.cog = self
+        
+    def cog_unload(self):
+        self.bot.help_command = self._original_help_command
+
+
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
