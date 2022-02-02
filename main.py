04724f096a5e789ccd114031ce68ffbd802e352e
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-02-02 14:57:52 -0300

    adadadadad

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ current_time = datetime.now()
 tz_BR = pytz.timezone('America/Sao_Paulo') 
 datetime_BR = datetime.now(tz_BR)
 
-
-
-
 @bot.event
 async def on_ready():
   bot.load_extension('cogs.')
@@ -38,12 +35,19 @@ for filename in os.listdir('Cogs'):
   if filename.endswith('.py'):
     bot.load_extension(f'Cogs.{filename[:-3]}')
 
+@bot.event
+async def on_ready():
+
+
   print ('===============================')
   print (f'Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
   #print (len(bot.guilds))
   print ('===============================')
 
-
+  ligado.start()
+  
+  bot.loop.create_task(status_task())
+  ligado.stop()
 
 @tasks.loop(seconds=11)
 async def ligado():
@@ -61,13 +65,6 @@ async def status_task():
         await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching,
                 name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.online)
         await asyncio.sleep(1800)
-
-@bot.event
-async def on_ready():
-  ligado.start()
-  
-  bot.loop.create_task(status_task())
-  ligado.stop()
 
 
 @tasks.loop(hours = 8)
