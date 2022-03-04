1bebc5cd07cbd6a595c86944782a14921328240d
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-03 21:53:04 -0300

    sfsffsfsfs

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -25,10 +25,6 @@ class PermOnly(commands.Cog, name = "Permonly"):
         await ctx.channel.purge(limit = amount + 1)
 
 
-    @commands.command()
-    async def ping(self, ctx):
-        latency = round(self.bot.latency * 1000)
-        await ctx.reply(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
 
 
 
--- Cogs/calc.py
+++ Cogs/calc.py
@@ -25,19 +25,10 @@ class Calculators(commands.Cog, name = "calculator"):
             result = c.convert ('USD', 'BRL', 1)
             await ctx.reply(content = f'Um dólar equivale atualmente a R${result:.3}')
 
-    @commands.command(help = 'Comando de ping(criado apenas para efeitos de teste)', aliases = ['p'])
-    async def pingnet(self, ctx):
-        async with ctx.typing():
-            servers = []
-            threads = None
-
-            s = speedtest.Speedtest()
-            s.get_servers(servers)
-            s.get_best_server()
-            s.download(threads=threads)
-            s.upload(pre_allocate=False,threads=threads)
-            
-            await ctx.reply (s.results.share())
+    @commands.command(help = 'Ping do bot com a API', aliases = ['p'])
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.reply(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
 
 
 def setup(bot):
