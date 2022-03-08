07b60fc53d09f56d4cc2ce0bb9f0e4a79d81830c
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-08 14:14:23 -0300

    asd

--- Cogs/util.py
+++ Cogs/util.py
@@ -39,7 +39,7 @@ class Util(commands.Cog, name = "Utility Commands"):
             usuario = ctx.author
 
             memberAvatar = usuario.avatar.url
-            avaEmbed = disnake.Embed(title = f"Avatar de {usuario.name}", color=0xf98b3c, description= '[Link](memberAvatar)')
+            avaEmbed = disnake.Embed(title = f"Avatar de {usuario.name}", color=0xf98b3c, description= f'Link: {memberAvatar}')
             avaEmbed.set_image(url=memberAvatar)
 
             await ctx.reply(embed = avaEmbed)
