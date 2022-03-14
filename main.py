b08f0bc39d90c9bbf2aa100dd09a11b22efa0ff5
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-14 13:03:13 -0300

    dasdad

--- main.py
+++ main.py
@@ -38,13 +38,13 @@ async def on_ready():
   print (f'Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
 
 
-  ligado.start()
+  #ligado.start()
   
   bot.loop.create_task(status_task())
-  ligado.stop()
+  #ligado.stop()
 
-@tasks.loop(seconds=1)
-async def ligado():
+#@tasks.loop(seconds=1)
+#async def ligado():
   user = [319963626108878848]
   for id in user:
     member = await bot.fetch_user(id)
