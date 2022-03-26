71bbdab0bb99b7c43e64d941d83cd72d17a317b2
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-25 22:55:31 -0300

    aaaa

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -19,7 +19,7 @@ class OwnerOnly(commands.Cog, name = "Owner Only"):
     async def dicio(self, ctx, *, palavra):
         async with ctx.typing():
             d = requests.get(f'https://significado.herokuapp.com/v2/{palavra}').json()
-            dados = d[{"partOfSpeech": "...","meanings": ["", ""]}]
+            dados = [{"partOfSpeech": "...","meanings": ["", ""]}]
             await ctx.reply(dados)
 
 
