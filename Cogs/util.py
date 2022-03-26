346beb76aa492b57e6c8f63a9dc29bc4317c87f7
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-26 14:20:10 -0300

    aa

--- Cogs/util.py
+++ Cogs/util.py
@@ -54,6 +54,7 @@ class Util(commands.Cog, name = "Utility Commands"):
         async with ctx.typing():
             d = requests.get(f'https://significado.herokuapp.com/v2/{palavra}').json()
             res = str(d[0]['meanings'])[1:-1]
+            ult = res.replace('[]','**')
             await ctx.reply(res)
 
 
