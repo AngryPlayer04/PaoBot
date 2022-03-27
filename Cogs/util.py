5c62d52c9b7dec0c96f02da812d59d556b52f2e0
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-03-26 23:35:01 -0300

    tadaaaa

--- Cogs/util.py
+++ Cogs/util.py
@@ -51,15 +51,17 @@ class Util(commands.Cog, name = "Utility Commands"):
         
     @commands.command(help = 'Busca pelo significado de uma palavra no dicionário.', aliases = ['dicionário', 'dicionario'])
     async def dicio(self, ctx, *, palavra):
-        async with ctx.typing():
-            d = requests.get(f'https://significado.herokuapp.com/v2/{palavra}').json()
-            res = str(d[0]['meanings'])[1:-1]
-            gen = str(d[0]['partOfSpeech'])
-            ult = res.replace('[','**').replace(']',':**').replace("'","").replace('.,','.').replace('.', '.\n')
-            dEmbed = disnake.Embed(title = palavra.capitalize(), color = 0xffb354, description = f'Gênero: {gen.capitalize()}')
-            dEmbed.set_thumbnail(url = 'https://purepng.com/public/uploads/large/purepng.com-dictionary-icon-android-lollipopsymbolsiconsgooglegoogle-iconsandroid-lollipoplollipop-iconsandroid-50-721522597173cj5xd.png')
-            dEmbed.add_field(name = 'Significado:', value = ult, inline = False)
-            await ctx.reply(embed = dEmbed) 
+        d = requests.get(f'https://significado.herokuapp.com/v2/{palavra}').json()
+
+        res = str(d[0]['meanings'])[1:-1]
+        gen = str(d[0]['partOfSpeech'])
+
+        ult = res.replace('[','**').replace(']',':**').replace("'","").replace('.,','.').replace('.', '.\n')
+        dEmbed = disnake.Embed(title = palavra.capitalize(), color = 0xffb354, description = f'Gênero: {gen.capitalize()}')
+        dEmbed.set_thumbnail(url = 'https://purepng.com/public/uploads/large/purepng.com-dictionary-icon-android-lollipopsymbolsiconsgooglegoogle-iconsandroid-lollipoplollipop-iconsandroid-50-721522597173cj5xd.png')
+        dEmbed.add_field(name = 'Significado:', value = ult, inline = False)
+        
+        await ctx.reply(embed = dEmbed) 
 
 
     @commands.Cog.listener()
