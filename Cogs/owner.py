4ea1c85340a489d3c872ce70b6c9f00056d4411b
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-04-09 14:58:13 -0300

    aaaaa

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -19,7 +19,7 @@ class Owner(commands.Cog, name = "Owner"):
             li = re['link']
 
             oEmbed = disnake.Embed(title = '\u200b', color = 0xffb354, description = f'[Link do log]({li})')
-            oEmbed.set_author(name = f'{self.bot.user.name} logs')
+            oEmbed.set_author(name = 'Pão Bot logs', icon_url = 'https://cdn.discordapp.com/avatars/850123093077917716/2fe303ab1bf685becf029d72834b0f16.png')
             oEmbed.add_field(name ='\u200b', value = f'```py \n {res} \n ```', inline=False)
             oEmbed.set_thumbnail(url = 'https://cdn-icons-png.flaticon.com/512/2125/2125009.png')
 
