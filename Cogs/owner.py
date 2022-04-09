c39832dd3e066f80c8b3556e1e7a08bf27597bfa
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-04-08 21:51:08 -0300

    aaa

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -14,7 +14,7 @@ class Owner(commands.Cog, name = "Owner"):
         async with ctx.typing():
             
             re = requests.get("https://discloud.app/api/v2/app/850123093077917716/logs", headers={"api-token": token}).json()
-            await ctx.reply(f'{re.logs}')
+            await ctx.reply(f'{re}')
 
     @commands.command(help = 'Reinicia o bot(*Apenas o dono do bot pode utilizar este comando*)', aliases = ['reiniciar', 'r'])
     @commands.is_owner()
