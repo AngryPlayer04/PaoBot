53a43e28d27adad8c6d5fe2108ec728d3c3794f3
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-04-09 14:22:28 -0300

    sada

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -17,6 +17,7 @@ class Owner(commands.Cog, name = "Owner"):
             re = requests.get("https://discloud.app/api/v2/app/850123093077917716/logs", headers={"api-token": token}).json()
             res = re['logs']
             li = re['link']
+            
             oEmbed = disnake.Embed(title = '\u200b', color = 0xffb354, description = f'[Link do log]({li})')
             oEmbed.set_author(name = f'{self.bot.user.name} logs', icon_url=str(bot.user.avatar_url))
             oEmbed.add_field(name ='\u200b', value = f'```py \n {res} \n ```', inline=False)
