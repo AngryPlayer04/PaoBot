a5363beff1789a59683379a6a375a02359519659
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-05-05 12:03:01 -0300

    fsfghjk

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -33,12 +33,11 @@ class Owner(commands.Cog, name = "Owner"):
     @commands.command(help = 'Status do bot')
     @commands.is_owner()
     async def status(self, ctx):
-        async with ctx.typing():
-            async def get_data():
-                async with aiohttp.ClientSession() as ses:
-                    async with ses.get("https://discloud.app/api/v2/app/850123093077917716", headers={"api-token": token}) as res:
-                        await ctx.reply(res.json())
-                        return res.close()
+        async def get_data():
+            async with aiohttp.ClientSession() as ses:
+                async with ses.get("https://discloud.app/api/v2/app/850123093077917716", headers={"api-token": token}) as res:
+                    await ctx.reply(res.json())
+                    ses.close()
 
 
     @commands.command(help = 'Reinicia o bot(*Apenas o dono do bot pode utilizar este comando*)', aliases = ['reiniciar', 'r'])
