9c5dd59b66f5b877f3358bdd2b5c80dcea4fac60
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-06-10 20:50:00 -0300

    sfsfsfsfs

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -22,8 +22,8 @@ class Calculators(commands.Cog, name = "Calculators"):
         async with ctx.typing():
             async with CurrencyApi() as session:
                 data = await session.convert('usd', 'brl')
-                
-            await ctx.reply(content = f'Um dólar equivale atualmente a R${data:.2f}')
+                await ctx.reply(content = f'Um dólar equivale atualmente a R${data:.2f}')
+            await session.close_session()
 
     @commands.command(help = 'Ping do bot com a API do Discord', aliases = ['p'])
     async def ping(self, ctx):
