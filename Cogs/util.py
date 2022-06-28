fb158f8c484d0ca7335b25aa94e68860c136fdab
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-06-28 13:42:06 -0300

    ticket command full functional

--- Cogs/util.py
+++ Cogs/util.py
@@ -109,18 +109,12 @@ class Util(commands.Cog, name = "Utility"):
         canal = disnake.utils.get(ctx.guild.text_channels, name = 'ticket-logs')
         
         if canal:
-            print('existe')
             mensagem = await chan.fetch_message(chan.last_message_id)
-            print('pegou a msg')
             await canal.send(f'De {mensagem.author}: \n{mensagem.content}')
-            print('enviou a msg')
             await chan.delete()
-            print('apagou o canal')
 
         else:
-
             lg = await disnake.Guild.create_text_channel(ctx.guild, name = 'ticket-logs', overwrites= permissao2)
-            print('passou pra ca')
             mensagem = await chan.fetch_message(chan.last_message_id)
             await lg.send(f'De {mensagem.author}: \n{mensagem.content}')
             await chan.delete()
