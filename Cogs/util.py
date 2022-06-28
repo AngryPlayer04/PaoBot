097720a6660d1a05130dc892ec1374a2f524de13
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-06-27 21:36:33 -0300

    daaaaaaaafg

--- Cogs/util.py
+++ Cogs/util.py
@@ -88,7 +88,7 @@ class Util(commands.Cog, name = "Utility"):
 
     @commands.command(help = 'Comando para abrir Tickets')
     async def ticket(self, ctx):
-        await disnake.Guild.create_text_channel(ctx.guild, name = f'{ctx.author}', overwrites= ctx.author)
+        await disnake.Guild.create_text_channel(ctx.guild, name = f'{ctx.author}', overwrites= [ctx.author])
         await ctx.send(f'{ctx.author.mention}Envie aqui a sua dúvida ou sugestão')
         #await asyncio.sleep(80)
         #await chan.delete()
