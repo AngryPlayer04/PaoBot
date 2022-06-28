c10d495204ba900ca93bad8225024e3dc786de39
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-06-27 23:54:19 -0300

    sfsfhgjk

--- Cogs/util.py
+++ Cogs/util.py
@@ -105,7 +105,7 @@ class Util(commands.Cog, name = "Utility"):
         await chan.send(f'{ctx.author.mention} envie aqui a sua dúvida ou sugestão')
         await asyncio.sleep(20)
 
-        canal = await disnake.Guild.channels
+        canal = disnake.Guild.channels
         if 'Ticket-Logs' in canal:
             await chan.delete()
 
