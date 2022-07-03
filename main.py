d5bae37dcb6c24e91153e7f540850fd4b067e81b
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-07-03 00:03:30 -0300

    daddad

--- main.py
+++ main.py
@@ -67,10 +67,10 @@ async def tempo_task():
   tz_SP = pytz.timezone('America/Sao_Paulo') 
   datetime_SP = datetime.now(tz_SP) 
   tempo = datetime_SP.strftime("%H:%M")
+  session = aiohttp.ClientSession()
   if tempo == '00:03':
-    session = aiohttp.ClientSession()
-    
     po = await session.post("https://discloud.app/api/v2/app/850123093077917716/restart", headers={"api-token": apitoken}).json()
-    await session.close()
+    
+  await session.close()
 
 bot.run(token)
