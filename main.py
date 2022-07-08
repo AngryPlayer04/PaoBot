fa0dfd0c6d00e3b8d9cf32466cd7115ff8c85de1
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-07-08 18:19:14 -0300

    update main.py

--- main.py
+++ main.py
@@ -75,12 +75,13 @@ async def tempo_task():
   tz_SP = pytz.timezone('America/Sao_Paulo') 
   datetime_SP = datetime.now(tz_SP) 
   tempo = datetime_SP.strftime("%H:%M")
-  hora = '18:15'
+  hora = '18:20'
   if tempo == hora:
     print('o tempo bateu')
   if tempo != hora:
     print('nao é hora')
     
-    
+ 
+ 
 
 bot.run(token)
