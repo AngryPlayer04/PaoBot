d9dadfae9d176937df16ae324eeaadf240616a65
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-07-15 13:21:19 -0300

    back to online

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def on_ready():
 async def status_task():
     while True:
         await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching,
-                name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.do_not_disturb)
+                name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.online)
         await asyncio.sleep(1800)
 
 
