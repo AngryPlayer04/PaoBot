5b5eee27796aa178a22080820464c81eea1800eb
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-08-26 12:40:07 -0300

    test

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -5,10 +5,11 @@ class AdminOnly(commands.Cog, name = "Admin"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(help = 'Limpa a quantidade indicada de mensagens do canal, sendo 5 por padrão', aliases = ['limpar', 'clean'])
+    @commands.slash_command(help = 'Limpa a quantidade indicada de mensagens do canal, sendo 5 por padrão')
     @commands.has_guild_permissions(manage_messages = True)
     async def clear(self, inter, amount = 5):
         await inter.channel.purge(limit = amount + 1)
+        await inter.response.send_message(f'Foram excluídas {amount} mensagens!', ephemeral = True)
 
 
     @commands.Cog.listener()
