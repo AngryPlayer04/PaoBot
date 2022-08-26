2e919d8440909255bc54936030dcef0a39609e21
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-08-26 01:28:02 -0300

    update calc.py

--- Cogs/calc.py
+++ Cogs/calc.py
@@ -1,4 +1,4 @@
-from discord.ext import commands
+from disnake.ext import commands
 import numexpr as ne
 from datetime import date
 import aiohttp
@@ -20,7 +20,7 @@ class Calculators(commands.Cog, name = "Calculators"):
             await ctx.reply(f"Desculpe, eu não posso calcular `{express}` ou ocorreu um erro desconhecido.")
 
 
-    @commands.command(help = 'Ping do bot com a API do Discord', aliases = ['p'])
+    @commands.command(help = 'Ping do bot com a API do disnake', aliases = ['p'])
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
         await ctx.reply(f'Pong! <a:paopula:858815343072903178> `{latency}ms` ')
