c1b674f2ad3b39662770e2bc772a93e7ca9bdd25
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-10-27 14:11:55 -0300

    update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ intents = disnake.Intents.default()
 intents.message_content = True
 
 # The bot
-bot = commands.Bot(intents = intents, case_insensitive = True, sync_commands_debug= True)
+bot = commands.Bot(intents = intents, sync_commands_debug= True)
 
 tz_BR = pytz.timezone('America/Sao_Paulo') 
 datetime_BR = datetime.now(tz_BR)
