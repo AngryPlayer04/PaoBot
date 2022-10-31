64d258838b2cdffeacf5bc8404932067382ba56e
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-10-31 15:16:37 -0300

    misstyped

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -28,7 +28,7 @@ class owner(commands.Cog, name = "Owner"):
         li = rei['url']
 
         oEmbed = disnake.Embed(title = 'Log:', color = 0xffb354, description = f'[Link do log]({li})')
-        oEmbed.set_auteor(name = 'Pão Bot', icon_url = 'https://cdn.disnakeapp.com/avatars/850123093077917716/2fe303ab1bf685becf029d72834b0f16.png')
+        oEmbed.set_author(name = 'Pão Bot', icon_url = 'https://cdn.disnakeapp.com/avatars/850123093077917716/2fe303ab1bf685becf029d72834b0f16.png')
         oEmbed.add_field(name ='\u200b', value = f'```{res}```', inline=False)
         oEmbed.set_thumbnail(url = 'https://cdn-icons-png.flaticon.com/512/2125/2125009.png')
 
